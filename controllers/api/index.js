2b3da8559998137bacc8d73f43a4d156f2b1ebc6
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2023-07-24 19:22:05 -0400

    troubleshoot

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const commentRoutes = require('./comments-Routes');
+const commentRoutes = require('./comment-routes');
 
 // Middleware
 router.use('/users', userRoutes);
